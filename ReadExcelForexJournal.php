e785268a96e58cf35c2ee24ece276306e4ab9fc1
BaneleMlamleli <mlamlelibanele@yahoo.com>
2021-11-05 02:12:52 +0200

    Added sql script to delete the table then create it, after that the newly created table will be inserted with data.

--- ReadExcelForexJournal.php
+++ ReadExcelForexJournal.php
@@ -1,13 +1,25 @@
 <?php
-    //require 'dbconnection.php';
-    require 'vendor/autoload.php';
+    require ("dbconnection.php");
+    require ("vendor/autoload.php");
 
     $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('Records.xlsx');   // Loading the Excel file that contains the sheet that I will
-    $sheet = $spreadsheet->getSheetByName("Test");  //getting the specific sheet I want to read data from
+    $sheet = $spreadsheet->getSheetByName("Journal (2)");  //getting the specific sheet I want to read data from
     
     // Store data from the read sheet to the variable in the form of Array
     $data = array(1,$sheet->toArray(null,true,true,true)); 
     
     // Display the sheet content 
-    var_dump($data);
+    //var_dump($data); 	 	 	 	 	 	 	 	 	 	 	
+
+    foreach($data[1] as $key){
+        $insertRecordStmt = "INSERT INTO `journalrecord` (`DateAndTime`, `Timeframe`, `Symbol`, `BuyOrSell`, `LotSize`, `EntryPrice`, `Stoploss`, `TakeProfit`, `StopLossPips`, `TakeProfitPips`, `ReasonForEntry`, `EntryType`, `Grade`, `TradeStatus`, `RiskReward`, `MonetaryValue`, `BeforeScreenshot`, `AfterScreenshot`, `Comments`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        if(mysqli_stmt_prepare($stmt, $insertRecordStmt)){
+            mysqli_stmt_bind_param($stmt, "ssssddddddsssssdsss", $key['A'], $key['B'], $key['C'], $key['D'], $key['E'], $key['F'], $key['G'], $key['H'], $key['I'], $key['J'], $key['K'],$key['L'], $key['M'], $key['N'], $key['O'], $key['P'], $key['Q'], $key['R'], $key['S']);
+            mysqli_stmt_execute($stmt);
+            echo '<script type=text/javascript>alert(\"Journal details successfully inserted\")</script>';
+        }else{
+            echo '<script type=text/javascript>alert(\"Error! Journal details were not inserted\")</script>';
+        }
+    }
 ?>
--- dbconnection.php
+++ dbconnection.php
@@ -20,5 +20,21 @@
     if ($conn->connect_error) {
         echo "<script type=text/javascript>alert('Database Connection failed')</script>";
         die($conn->connect_error);
-    } 
+    }
+
+    //dropping the table
+    if ($conn->query("DROP TABLE `journalrecord`")) {
+        echo "<script type=text/javascript>alert('Table \'journalrecord\' has been deleted/dropped successfully.)</script>";
+     }elseif ($conn->errno) {
+        echo "<script type=text/javascript>alert('Table \'journalrecord\' was not deleted/dropped.)</script>";
+     }
+
+     //creating the table again
+    $sqlCreateTable = "CREATE TABLE `forexjournalrecorder`.`journalrecord` ( `Pk` INT NOT NULL AUTO_INCREMENT , `DateAndTime` VARCHAR(30) NOT NULL , `Timeframe` VARCHAR(10) NOT NULL , `Symbol` VARCHAR(6) NOT NULL , `BuyOrSell` VARCHAR(5) NOT NULL , `LotSize` DOUBLE NOT NULL , `EntryPrice` DOUBLE NOT NULL , `Stoploss` DOUBLE NOT NULL , `TakeProfit` DOUBLE NOT NULL , `StopLossPips` DOUBLE NOT NULL , `TakeProfitPips` DOUBLE NOT NULL , `ReasonForEntry` VARCHAR(55) NOT NULL , `EntryType` VARCHAR(55) NOT NULL , `Grade` VARCHAR(20) NOT NULL , `TradeStatus` VARCHAR(20) NOT NULL , `RiskReward` VARCHAR(5) NOT NULL , `MonetaryValue` DOUBLE NOT NULL , `BeforeScreenshot` VARCHAR(200) NOT NULL , `AfterScreenshot` VARCHAR(200) NOT NULL , `Comments` VARCHAR(500) NOT NULL , PRIMARY KEY (`Pk`)) ENGINE = InnoDB;";
+
+    if ($conn->query($sqlCreateTable) === TRUE) {
+        echo "<script type=text/javascript>alert('Table \'journalrecord\' was created successfully.)</script>";
+    } else {
+        echo "<script type=text/javascript>alert('Table \'journalrecord\' was not created.)</script>";
+    }
 ?>
